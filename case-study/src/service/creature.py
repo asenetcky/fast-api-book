136d9a2c01a3cd7436f458a8fb2abece55b3185c
asenetcky <asenetcky@gmail.com>
2025-05-02 21:17:40 -0400

    feat: add service

---
+++ case-study/src/service/creature.py
@@ -0,0 +1,26 @@
+from models.creature import Creature
+import fake.creature as data
+
+
+def get_all() -> list[Creature]:
+    return data.get_all()
+
+
+def get_one(name: str) -> Creature | None:
+    return data.get(id)  # okay is the id and name mismatch on purpose?
+
+
+def create(creature: Creature) -> Creature:
+    return data.create(creature)
+
+
+def replace(creature: Creature) -> Creature:
+    return data.replace(id, creature)
+
+
+def modify(id, creature: Creature) -> Creature:
+    return data.modify(id, creature)
+
+
+def delete(id: str) -> bool:
+    return data.delete(id)
