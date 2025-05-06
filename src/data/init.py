d326eaeaf106602239ddbeaf4b9626bd11f70ce9
asenetcky <asenetcky@gmail.com>
2025-05-06 13:50:00 -0400

    fix: init.py

--- src/data/init.py
+++ src/data/init.py
@@ -7,7 +7,8 @@ from sqlite3 import connect, Connection, Cursor, IntegrityError
 conn: Connection | None = None
 curs: Cursor | None = None
 
-def get_db(name: str|None = None, reset: bool = False)
+
+def get_db(name: str | None = None, reset: bool = False):
     """Connect to SQLite database file"""
     global conn, curs
     if conn:
@@ -16,12 +17,13 @@ def get_db(name: str|None = None, reset: bool = False)
         conn = None
     if not name:
         name = os.getenv("CRYPTID_SQLITE_DB")
-        top_dir = Path(__file__).resolve().parents[1] # repo top
+        top_dir = Path(__file__).resolve().parents[1]  # repo top
         db_dir = top_dir / "db"
         db_name = "cryptid.db"
         db_path = str(db_dir / db_name)
         name = os.getenv("CRYPTID_SQLITE_DB", db_path)
-    conn = connect(name, check_same_thread=false)
+    conn = connect(name, check_same_thread=False)
     curs = conn.cursor()
+
 
 get_db()
